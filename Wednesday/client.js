acb57cdf5f2cf3bbb627d326da2b9949e31d24de
CJ Barnes <cristopher.barnes@gmail.com>
2021-08-18 20:17:43 -0400

    end of deck of cards

--- Wednesday/client.js
+++ Wednesday/client.js
@@ -6,14 +6,15 @@ let testCard = {
     value: [1 , 14],
     shown: false
 } // end testCard
-
+let deck = [];
 const createSuit = suitToCreate => {
-    console.log( 'in createSuit:', suitToCreate);
     //use loop to create all cards in a given suit
     for (let i = 1; i <=13; i++){
         let cardFace;
+        let cardValue = [i];
         if (i === 1){
             cardFace = 'Ace';
+            cardValue.push(14);
         } else if (i === 11){
             cardFace = 'Jack';
         } else if (i === 12){
@@ -25,15 +26,20 @@ const createSuit = suitToCreate => {
         }
         let newCard = {
             face: cardFace,
-            values: [ i ],
+            values: cardValue,
             suit: suitToCreate,
             shown: false //default value
         }
-        console.log('created:', newCard);
+        deck.push(newCard)
     } //end for
 } //end createSuit
+
 //create Deck of Cards
-createSuit('Hearts');
-createSuit('Spades');
-createSuit('Diamonds');
-createSuit('Clubs');
+const createDeck = () => {
+    const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+    for (let i = 0; i <suits.length; i++){
+        createSuit(suits[i]);
+    }
+    console.log(deck);
+}
+createDeck();
